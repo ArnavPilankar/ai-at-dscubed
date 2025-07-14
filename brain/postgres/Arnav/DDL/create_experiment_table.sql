6dffa2b81b0fa1bc9f7171ab00a6d6d6cd2f51c1
ArnavPilankar <pilankararnav9@gmail.com>
2025-07-14 16:30:43 +0530

    Created DDL and DML file

---
+++ brain/postgres/Arnav/DDL/create_experiment_table.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA project_two;
+
+CREATE TABLE project_two.Arnav (
+    exp_id UUID PRIMARY KEY,
+    exp_name TEXT NOT NULL,
+    member_name TEXT NOT NULL,
+    dataset_used TEXT NOT NULL,
+    model_used TEXT NOT NULL,
+    model_params JSON,
+    accuracy FLOAT,
+    remarks TEXT
+);
---
+++ brain/postgres/Arnav/DML/insert_experiments.sql
@@ -0,0 +1,10 @@
+INSERT INTO project_two.arnav (
+    exp_id,
+    exp_name,
+    member_name,
+    dataset_used,
+    model_used,
+    model_params,
+    accuracy,
+    remarks
+)
